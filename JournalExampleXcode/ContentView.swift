994b38176d5b24adca662768ffa66c4ab654ac16
ramirezi29 <ramirezfranciscoivan@gmail.com>
2020-04-17 14:43:03 -0700

    sample commit

---
+++ JournalExampleXcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JournalExampleXcode
+//
+//  Created by Ivan Ramirez on 4/17/20.
+//  Copyright © 2020 com.cacutsapps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+// Random Line
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JournalExampleXcode/RandomFile.swift
@@ -0,0 +1,10 @@
+//
+//  RandomFile.swift
+//  JournalExampleXcode
+//
+//  Created by Ivan Ramirez on 4/17/20.
+//  Copyright © 2020 com.cacutsapps. All rights reserved.
+//
+
+import Foundation
+//sample 
