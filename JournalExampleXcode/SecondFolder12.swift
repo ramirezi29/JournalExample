d262a650d73f303799a2aaa64d716d990ca9ae25
ramirezi29 <ramirezfranciscoivan@gmail.com>
2020-04-17 14:51:52 -0700

    Contains new folder titled SecondFOlder12

---
+++ JournalExampleXcode/SecondFolder12.swift
@@ -0,0 +1,15 @@
+//
+//  SecondFolder12.swift
+//  JournalExampleXcode
+//
+//  Created by Ivan Ramirez on 4/17/20.
+//  Copyright © 2020 com.cacutsapps. All rights reserved.
+//
+
+import CloudKit
+import Foundation
+//sample sample
+
+//
+
+///
